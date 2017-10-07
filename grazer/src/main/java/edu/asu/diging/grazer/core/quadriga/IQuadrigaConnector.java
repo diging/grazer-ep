bdc1f373704410897a8bff1a25d1aa7155caa41b
Julia Damerow <julia.damerow@googlemail.com>
2017-10-06 17:12:11 -0700

    [QUAD-262] poll quadriga for person list results

--- grazer/src/main/java/edu/asu/diging/grazer/core/quadriga/IQuadrigaConnector.java
+++ grazer/src/main/java/edu/asu/diging/grazer/core/quadriga/IQuadrigaConnector.java
@@ -5,14 +5,17 @@ import java.util.List;
 import java.util.Map;
 
 import edu.asu.diging.grazer.core.model.IConcept;
+import edu.asu.diging.grazer.core.quadriga.impl.PollResponse;
 import edu.asu.diging.grazer.core.quadriga.impl.TransformationResponse;
 
 public interface IQuadrigaConnector {
 
-    List<IConcept> getPersons();
+    PollResponse getPersons();
 
     TransformationResponse getTransformedNetworks(String transformationName, Map<String, String> properties) throws IOException;
 
     TransformationResponse checkForResult(TransformationResponse response);
+
+    List<IConcept> checkPersonsResult(String url);
 
 }
