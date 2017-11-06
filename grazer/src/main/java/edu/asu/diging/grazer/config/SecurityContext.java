1ed55f70abdcf0f81428df38854318b7726ccd52
mshah18 <mshah18@sols-jessima-06.dhcp.asu.edu>
2017-11-06 11:40:08 -0700

    [EPGR-7] addressed review comments: changed http authentication in spring-security.xml to java config

--- grazer/src/main/java/edu/asu/diging/grazer/config/SecurityContext.java
+++ grazer/src/main/java/edu/asu/diging/grazer/config/SecurityContext.java
@@ -24,25 +24,25 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.formLogin()
-                .loginPage("/")
-                .loginProcessingUrl("/login/authenticate")
-                .failureUrl("/?error=bad_credentials")
+                .loginPage("/login")
+                //.loginProcessingUrl("/login/authenticate")
+                .failureUrl("/loginfailed")
                 // Configures the logout function
                 .and()
                 .logout()
                 .deleteCookies("JSESSIONID")
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
-                .and().exceptionHandling().accessDeniedPage("/403")
+                .and().exceptionHandling().accessDeniedPage("/403");
                 // Configures url based authorization
-                .and()
-                .authorizeRequests()
+                //.and()
+                //.authorizeRequests()
                 // Anyone can access the urls
-                .antMatchers("/", "/persons/**", "/person/**", "/concept/**", "/query**", "/info", "/resources/**", "/login/authenticate", "/rest/**",
-                        "/logout").permitAll()
+                //.antMatchers("/", "/persons/**", "/person/**", "/concept/**", "/query**", "/info", "/resources/**", "/login/authenticate", "/rest/**",
+                //        "/logout").permitAll()
                 // The rest of the our application is protected.
-                .antMatchers("/users/**", "/admin/**", "/api/**").hasRole("ADMIN")
-                .anyRequest().hasRole("USER").and().httpBasic();
+                //.antMatchers("/users/**", "/admin/**", "/api/**").hasRole("ADMIN")
+                //.anyRequest().hasRole("USER").and().httpBasic();
     }
 
     @Bean
